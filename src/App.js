b7382afab0341b964f9e957f3a59b0358694de03
unknown <alineoliveiravaz@gmail.com>
2021-11-12 20:50:33 -0300

    oi

--- src/App.js
+++ src/App.js
@@ -1,12 +1,35 @@
 import React from "react";
+import { render } from "react-dom";
 import Button from "./Components/Button/button";
+import Checkbox from "./Components/Checkbox/checkbox";
 
-const App = () => {
-  return (
-    <div>
-      <Button>Hello World</Button>
-    </div>
+const { useState } = React;
+function App () {
+  const [checked, setChecked] = useState(false);
+  const toggleChecked = () => setChecked(value => !value);
+    return (
+      <><div>
+        <label>
+          <Checkbox
+            checked={this.state.checked}
+            onChange={this.handleCheckboxChange} />
+          <span>Label Text</span>
+        </label>
+      </div><div>
+          <Button>Hello World</Button>
+        </div><div>
+          <form action="/action_page.php">
+            <input type="text" placeholder="Search.." name="search" />
+            <button type="submit">Submit</button>
+          </form>
+        </div><div>
+          <input type="checkbox" id="chkb" name="chkb" value="Name" />
+          <label htmlFor="Name"> Remember me</label>
+        </div></>   
+    ); 
   );
-};
+}
 
 export default App;
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
