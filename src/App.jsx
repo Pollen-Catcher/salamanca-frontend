f5e1f34408af4eb85e650775dc8b5858e285b501
Henrique de Rodrigues <tec.henriquedepaula@gmail.com>
2022-11-07 00:50:31 -0300

    Removendo contexto e rotas da aplicação

--- src/App.jsx
+++ src/App.jsx
@@ -1,72 +1,15 @@
-import PropTypes from 'prop-types'
-import { useContext } from 'react'
-import { useAuthState } from 'react-firebase-hooks/auth'
-import {
-  BrowserRouter as Router,
-  Route,
-  Routes,
-  useLocation,
-} from 'react-router-dom'
-import { Navigate } from 'react-router-dom'
+import { CssBaseline } from '@mui/material'
+import React from 'react'
 
-import { Projects, Sheets } from './components'
-import { FirebaseContext } from './contexts/firebaseContext'
-import { Layout, LoginPage } from './pages'
+import { GlobalContext } from './contexts'
+import { Router } from './Router'
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route
-          path="/login"
-          element={<LoginPage />}
-          render={(props) => <LoginPage {...props} />}
-        />
-        <Route
-          path="/"
-          element={<Layout />}
-          render={(props) => <Layout {...props} />}
-        >
-          <Route
-            index
-            element={
-              <RequireAuth>
-                <Projects />
-              </RequireAuth>
-            }
-          />
-          <Route
-            path="/home/:sheetId"
-            element={
-              <RequireAuth>
-                <Sheets />
-              </RequireAuth>
-            }
-          />
-        </Route>
-      </Routes>
-    </Router>
+    <GlobalContext>
+      <CssBaseline />
+      <Router />
+    </GlobalContext>
   )
 }
-
-function RequireAuth({ children }) {
-  const { auth } = useContext(FirebaseContext)
-  const [user, loading] = useAuthState(auth)
-  const location = useLocation()
-
-  if (loading) {
-    return <div>Loading...</div>
-  }
-
-  if (!user) {
-    return <Navigate to="/login" state={{ from: location }} replace />
-  }
-
-  return children
-}
-
-RequireAuth.propTypes = {
-  children: PropTypes.node,
-}
-
 export default App
