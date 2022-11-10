e12d279fe68947008857a5955bd745b133c3d3c3
Henrique de Rodrigues <tec.henriquedepaula@gmail.com>
2022-11-09 22:26:52 -0300

    Adicionando schemas para validação do formulário no frontend

---
+++ src/components/Forms/SignUpForm/schema/index.js
@@ -0,0 +1,15 @@
+import * as yup from 'yup'
+export const schema = yup.object().shape({
+  name: yup.string().required('Please enter your username'),
+  email: yup
+    .string()
+    .email('Please enter a valid email format')
+    .required('Please enter your e-mail'),
+  password: yup
+    .string()
+    .min(8, 'Password must contain at least 8 characters')
+    .required('Please enter your password'),
+  passwordConfirmation: yup
+    .string()
+    .oneOf([yup.ref('password'), null], 'Passwords must match'),
+})
