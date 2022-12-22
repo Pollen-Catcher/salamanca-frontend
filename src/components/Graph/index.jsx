ba3c7009331627898241c0706ccec182fce82496
Henrique de Paula Rodrigues <tec.henriquedepaula@gmail.com>
2022-12-21 22:49:28 -0300

    running mean 5 graph

--- src/components/Graph/index.jsx
+++ src/components/Graph/index.jsx
@@ -1,3 +1,5 @@
+import { LineController } from 'chart.js'
+import { TimeScale } from 'chart.js'
 import {
   CategoryScale,
   Chart as ChartJS,
@@ -17,123 +19,162 @@ import { FirebaseContext } from '../../contexts/Auth/firebaseContext'
 ChartJS.register(
   CategoryScale,
   LinearScale,
+  LineController,
+  LineElement,
   PointElement,
   LineElement,
   Title,
   Tooltip,
   Legend
 )
-import { pollensList } from '../../data/arrays'
-
 export const options = {
   responsive: true,
   plugins: {
     legend: {
-      position: 'bottom',
+      position: 'right',
     },
     title: {
       display: true,
-      text: 'Chart.js Line Chart',
+      text: 'Running mean',
     },
   },
 }
-const datasets = []
-// cria cores para cada pollen para ser adicionado posteriormente
-pollensList.forEach((pollen) => {
-  const label = pollen
+function getDailySums({ pollenData, pollenName, factor }) {
+  const pollens = pollenData?.map((pollen) => {
+    return {
+      Acer: pollen[pollenName],
+      date: pollen.date,
+    }
+  })
+  const dailySums = {}
+  pollens?.forEach(({ Acer, date }) => {
+    let soma = 0
+    for (const el in Acer) soma += Acer[el]
+    dailySums[date] = soma * factor // fator de correção aplicado
+  })
+  return dailySums
+}
+function getDailyGraph(pollenData, pollenName, factor) {
   const color = `${Math.random() * 256},${Math.random() * 256},${
     Math.random() * 256
   }`
-  // gera dados aleatórios de coletas enquanto o banco de dados ainda não está configurado
-  function generateData(n) {
-    const data = []
-    for (let i = 0; i < n; i++) {
-      const num = Math.random() * 256
-      data.push(num)
+  const dailySums = getDailySums({ pollenData, factor, pollenName })
+  const data = {
+    labels: Object.keys(dailySums),
+    datasets: [
+      {
+        label: pollenName,
+        fill: false,
+        backgroundColor: `rgba(${color},0.4)`,
+        borderColor: `rgba(${color},1)`,
+        borderCapStyle: 'butt',
+        borderDash: [],
+        borderDashOffset: 0.0,
+        borderJoinStyle: 'miter',
+        pointBorderColor: `rgba(${color},1)`,
+        pointBackgroundColor: '#fff',
+        pointBorderWidth: 1,
+        pointHoverRadius: 5,
+        pointHoverBackgroundColor: `rgba(${color},1)`,
+        pointHoverBorderColor: 'rgba(220,220,220,1)',
+        pointHoverBorderWidth: 2,
+        pointRadius: 1,
+        pointHitRadius: 10,
+        data: Object.values(dailySums),
+      },
+    ],
+  }
+  return data
+}
+function getRunningMean({ pollenData, pollenName, factor, n = 5 }) {
+  const color = `${Math.random() * 256},${Math.random() * 256},${
+    Math.random() * 256
+  }`
+  const dailySums = getDailySums({ factor, pollenData, pollenName })
+  let sum = 0
+  const values = Object.values(dailySums)
+  const keys = Object.keys(dailySums)
+  const runningMean = {}
+  for (let i = 0; i < values.length; i++) {
+    if (i > n) {
+      sum -= values.shift()
     }
-    return data
+    sum += values[i]
+    runningMean[keys[i]] = sum / n
   }
-  const data = generateData(7)
-  const dataset = {
-    label,
-    fill: false,
-    backgroundColor: `rgba(${color},0.4)`,
-    borderColor: `rgba(${color},1)`,
-    borderCapStyle: 'butt',
-    borderDash: [],
-    borderDashOffset: 0.0,
-    borderJoinStyle: 'miter',
-    pointBorderColor: `rgba(${color},1)`,
-    pointBackgroundColor: '#fff',
-    pointBorderWidth: 1,
-    pointHoverRadius: 5,
-    pointHoverBackgroundColor: `rgba(${color},1)`,
-    pointHoverBorderColor: 'rgba(220,220,220,1)',
-    pointHoverBorderWidth: 2,
-    pointRadius: 1,
-    pointHitRadius: 10,
-    data,
+  const data = {
+    labels: Object.keys(runningMean),
+    datasets: [
+      {
+        label: pollenName,
+        fill: false,
+        backgroundColor: `rgba(${color},0.4)`,
+        borderColor: `rgba(${color},1)`,
+        borderCapStyle: 'butt',
+        borderDash: [],
+        borderDashOffset: 0.0,
+        borderJoinStyle: 'miter',
+        pointBorderColor: `rgba(${color},1)`,
+        pointBackgroundColor: '#fff',
+        pointBorderWidth: 1,
+        pointHoverRadius: 5,
+        pointHoverBackgroundColor: `rgba(${color},1)`,
+        pointHoverBorderColor: 'rgba(220,220,220,1)',
+        pointHoverBorderWidth: 2,
+        pointRadius: 1,
+        pointHitRadius: 10,
+        data: Object.values(runningMean),
+      },
+    ],
   }
-  datasets.push(dataset)
-})
-function getPollens(dateInitial, dateFinal) {
-  if (!dateFinal) {
-    const data = generateData(24)
-    const dataset = {
-      labels: [
-        '0',
-        '1',
-        '2',
-        '3',
-        '4',
-        '5',
-        '6',
-        '7',
-        '8',
-        '9',
-        '10',
-        '11',
-        '12',
-        '13',
-        '14',
-        '15',
-        '16',
-        '17',
-        '18',
-        '19',
-        '20',
-        '21',
-        '22',
-        '23',
-      ],
-      data,
-    }
-  }
+  return data
 }
-const data2 = {
-  labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
-  datasets,
-}
-const getAcer = async (query) => {
-  try {
-    const data = await getDocs(query)
-    return data
-  } catch (error) {
-    console.error(error)
-  }
-}
-async function Graph() {
-  const { db } = useContext(FirebaseContext)
+function Graph() {
+  const [factor, setFactor] = useState([0.486])
   const [data, setData] = useState()
+  const { db } = useContext(FirebaseContext)
   const AcerQuery = query(collectionGroup(db, 'days'))
-  useEffect(async () => {
-    const data = await getAcer(AcerQuery)
+  const [pollens] = useCollectionDataOnce(AcerQuery)
+  useEffect(() => {
+    //const data = getDailyGraph(pollens, 'Acer', factor)
+    const data = getRunningMean({
+      factor,
+      pollenData: pollens,
+      pollenName: 'Acer',
+    })
     setData(data)
   }, [])
-  console.log(data)
+  useEffect(() => {
+    //const data = getDailyGraph(pollens, 'Acer', factor)
+    const data = getRunningMean({
+      factor,
+      pollenData: pollens,
+      pollenName: 'Acer',
+    })
+    setData(data)
+  }, [pollens, factor])
   return (
-    <div className="flex items-center justify-center">
-      <Line options={options} data={data2} width={200} height={200} />
+    <div className="flex flex-col justify-center">
+      <div className="px-8">
+        <p className="text-lg font-bold text-black">Factor : {factor}</p>
+        <input
+          type="range"
+          min={0}
+          max={1}
+          step={0.1}
+          value={factor}
+          onChange={(el) => setFactor(el.target.value)}
+        />
+      </div>
+      <div className="">
+        {data ? (
+          <Line options={options} data={data} width={2} height={2} />
+        ) : (
+          <div>
+            <p>Sem data</p>
+          </div>
+        )}
+      </div>
     </div>
   )
 }
